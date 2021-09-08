caeb5c060b10ebc42e7c132ccdaebf23ce688e1c
zutu <51734083+ZuTuu@users.noreply.github.com>
2021-09-08 20:37:02 +0700

    Update threadbot.py

--- threadbot.py
+++ threadbot.py
@@ -79,4 +79,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run("Nzg3MjM2NTAzOTk4MTY5MTQ4.X9SBIA.EuvxGwuJHqhOjCFAMM4SVsyeL_U")
+client.run("token")
